20e25fe435254784394985d1b6ea1adc62641f45
Eyal Posener <posener@gmail.com>
2017-04-28 12:51:58 +0300

    Add pipe doc, nicer logging

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -1,8 +1,19 @@
+// pipe provides a function that creates two paired in-memory
+// net connections: objects that implements the `net.Conn` interface.
+//
+// The standard library `net.Pipe() (c1, c2 net.Conn)` function
+// creates two in-memory paired connections, which will return a
+// not-implemented error up on SetDeadline, SetReadDeadline or SetWriteDeadline
+// calls. This functionality is sometimes needed, and it is provided by
+// this package pipe implementation.
 package pipe
 
 import "net"
 
-func New(debugLog func(...interface{})) (net.Conn, net.Conn) {
+// New creates a new pipe.
+// log is a log.Println-like function that can be set for debugging the
+// connection. It can be set to nil if no debug is needed.
+func New(log Println) (c1, c2 net.Conn) {
 	var (
 		s2c   = newBuffer()
 		c2s   = newBuffer()
@@ -10,8 +21,8 @@ func New(debugLog func(...interface{})) (net.Conn, net.Conn) {
 		sAddr = &address{"tcp", "5.6.7.8:12346"}
 	)
 
-	client := &conn{name: "server", in: c2s, out: s2c, local: sAddr, remote: cAddr, logger: debugLog}
-	server := &conn{name: "client", in: s2c, out: c2s, local: cAddr, remote: sAddr, logger: debugLog}
+	c1 = &conn{name: "client", in: s2c, out: c2s, local: cAddr, remote: sAddr, log: log}
+	c2 = &conn{name: "server", in: c2s, out: s2c, local: sAddr, remote: cAddr, log: log}
 
-	return client, server
+	return
 }
